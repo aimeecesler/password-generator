aa6d59d6842cf33148858560ce0eb4c710bafb43
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-02 16:25:37 -0400

    initialized an empty array for elligble characters called characterOptions

--- script.js
+++ script.js
@@ -7,47 +7,64 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function generatePassword(){
+
+
+function generatePassword() {
   // TODO: GENERATE A REAL PASSWORD AND REPLACE THE RETURN STRING WITH A REAL PASSWORD
- 
 
-  var passwordLength = prompt("How many characters would you like your password to contain? (must be a number between 8 and 128)")
+  var characterOptions = []
 
-  if(passwordLength > 128 || passwordLength < 8){
+  var passwordLength = prompt(
+    "How many characters would you like your password to contain? (must be a number between 8 and 128)"
+  );
+
+  if (passwordLength > 128 || passwordLength < 8) {
     alert("Invalid Entry: Password must be between 8 and 128 characters.");
     // TODO: how do I make it stop here and reset if they put in an invalid entry?
+  } else console.log("Password Length: " + passwordLength);
+
+  var containsLowercase = confirm(
+    "Would you like your password to contain lowercase letters?"
+  );
+  console.log("Contains lowercase: " + containsLowercase);
+
+  var containsUppercase = confirm(
+    "Would you like your password to contain UPPERCASE letters?"
+  );
+  console.log("Contains uppercase: " + containsUppercase);
+
+  var containsNumbers = confirm(
+    "Would you like your password to contain numbers?"
+  );
+  console.log("Contains numbers: " + containsNumbers);
+
+  var containsSpecialCharacters = confirm(
+    "Would you like your password to contain special characters?"
+  );
+  console.log("Contains special characters: " + containsSpecialCharacters);
+
+  if (
+    containsLowercase === false &&
+    containsUppercase === false &&
+    containsNumbers === false &&
+    containsSpecialCharacters === false
+  ) {
+    alert(
+      "Error: Your password must contain at least one of the following - lowercase letters, uppercase letters, numbers, special characters"
+    );
+    // TODO: how do I make it stop here and reset if they put in an invalid entry?
   }
-  else (
-    console.log("Password Length: " + passwordLength)
-  )
 
-  var containsLowercase = confirm("Would you like your password to contain lowercase letters?")
-  console.log("Contains lowercase: " + containsLowercase)
+  var passwordOptions = {
+    alphabet: "abcdefghijklmnopqrstuvwxyz",
+    numbers: "123456789",
+    specialCharacters: "!@#$%&*?+-/",
 
-  var containsUppercase = confirm("Would you like your password to contain UPPERCASE letters?")
-  console.log("Contains uppercase: " + containsUppercase)
-
-  var containsNumbers = confirm("Would you like your password to contain numbers?")
-  console.log("Contains numbers: " + containsNumbers)
-
-  var containsSpecialCharacters = confirm("Would you like your password to contain special characters?")
-  console.log("Contains special characters: " + containsSpecialCharacters)
-
-  if(containsLowercase === false && containsUppercase === false && containsNumbers === false && containsSpecialCharacters === false) {
-    alert("Error: Your password must contain at least one of the following - lowercase letters, uppercase letters, numbers, special characters")
-        // TODO: how do I make it stop here and reset if they put in an invalid entry?
   }
-  // passwordOptions {
-  //   alphabet: "abcdefghijklmnopqrstuvwxyz"
-  //   numbers: "123456789"
-  //   specialCharacters: "!@#$%&*?+-/"
-  // }
 
-
-   // return "THIS IS NOT MY FINAL PASSWORD"
+  // return "THIS IS NOT MY FINAL PASSWORD"
 }
 
 // Add event listener to generate button
